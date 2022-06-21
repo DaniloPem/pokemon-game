867ae10552dbdabd97b30c3705adc1c4cd0595a2
Danilo Pezo <danilo_97pem@hotmail.com>
2022-06-21 13:06:42 -0300

    Implementado o movimento do personagem

--- src/page/home/home.js
+++ src/page/home/home.js
@@ -5,16 +5,61 @@ const contexto = canvas.getContext("2d");
 const backgroundHome = new Image();
 backgroundHome.src = "../../../assets/imagem/backgroundHome.png";
 
+const spritePersonagem = new Image();
+spritePersonagem.src = "../../../assets/imagem/personagem.png";
+
 //Objetos
 const sprites = [];
 const cenario = {
   img: backgroundHome,
   x: 500,
   y: 650,
-  width: 3360,
-  height: 1920,
+  posicaoX: 0,
+  posicaoY: 0,
+  largura: 3360,
+  altura: 1920,
 };
 sprites.push(cenario);
+
+const personagem = {
+  img: spritePersonagem,
+  x: 0,
+  y: 0,
+  posicaoX: canvas.clientWidth / 2,
+  posicaoY: canvas.clientHeight / 2,
+  largura: 80,
+  altura: 80,
+};
+sprites.push(personagem);
+
+const camera = {
+  x: 500,
+  y: 650,
+  largura: canvas.clientWidth,
+  altura: canvas.clientHeight,
+  bordeEsquerdo: function () {
+    return this.x + this.largura * 0.25;
+  },
+  bordeDireito: function () {
+    return this.x + this.largura * 0.75;
+  },
+  bordeCima: function () {
+    return this.y + this.altura * 0.25;
+  },
+  bordeBaixo: function () {
+    return this.y + this.altura * 0.75;
+  },
+};
+
+//Movimento do personagem
+const keymap = {};
+
+document.addEventListener("keydown", (e) => {
+  keymap[e.key] = true;
+});
+document.addEventListener("keyup", (e) => {
+  keymap[e.key] = false;
+});
 
 //Rodando infinitamente
 const loop = () => {
@@ -24,11 +69,38 @@ const loop = () => {
 };
 
 //Pra fazer atualizacoes
-const update = () => {};
+const update = () => {
+  if (keymap["ArrowLeft"]) {
+    personagem.posicaoX -= 4;
+  }
+  if (keymap["ArrowRight"]) {
+    personagem.posicaoX += 4;
+  }
+  if (keymap["ArrowUp"]) {
+    personagem.posicaoY -= 4;
+  }
+  if (keymap["ArrowDown"]) {
+    personagem.posicaoY += 4;
+  }
+};
 
 //Desenhar funcoes na tela
 const render = () => {
-  contexto.drawImage(backgroundHome, 500, 650, 3360, 1920, 0, 0, 3360, 1920);
+  contexto.save();
+  sprites.forEach((sprite) => {
+    contexto.drawImage(
+      sprite.img,
+      sprite.x,
+      sprite.y,
+      sprite.largura,
+      sprite.altura,
+      sprite.posicaoX,
+      sprite.posicaoY,
+      sprite.largura,
+      sprite.altura
+    );
+  });
+  contexto.restore();
 };
 
 loop();
